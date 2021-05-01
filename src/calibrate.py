a2f693f3652d9ccee6b4bfb71b9a3438a65df30d
Brendan Duke <brendanw.duke@gmail.com>
2021-04-30 20:20:47 -0400

    calibrate.py

---
+++ src/calibrate.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+import click
+from picamera import PiCamera
+
+
+@click.group()
+def calibrate():
+    pass
+
+
+@click.command()
+@click.option("--output-path", type=str, required=True)
+def preview(output_path):
+    camera = PiCamera()
+
+    camera.start_preview()
+    while True:
+        time.sleep(2)
+        camera.capture(output_path)
+
+
+if __name__ == "__main__":
+    calibrate()
