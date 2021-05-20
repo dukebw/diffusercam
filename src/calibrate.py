2495d898caccaca6ba70b83eda4c07543eb05b2b
Brendan Duke <brendanw.duke@gmail.com>
2021-05-19 21:20:17 -0400

    Report start

--- src/calibrate.py
+++ src/calibrate.py
@@ -3,7 +3,12 @@ import psutil
 import time
 
 import click
-from picamera import PiCamera
+import numpy as np
+
+try:
+    from picamera import PiCamera
+except ModuleNotFoundError:
+    print("No picamera")
 from PIL import Image
 
 
@@ -25,9 +30,24 @@ def preview(output_path):
 
         camera.capture(output_path)
         img = Image.open(output_path)
+        print(np.array(img).min(), np.array(img).max())
         img.show()
 
 
+@click.command()
+@click.option("--input-path", type=str, required=True)
+@click.option("--output-path", type=str, required=True)
+def extract_calibration_image(input_path, output_path):
+    input_img = Image.open(input_path)
+    input_img = np.array(input_img)[:, :, 1]
+    input_img = input_img * (255.0 / input_img.max())
+    input_img = input_img.astype(np.uint8)
+
+    output_img = Image.fromarray(input_img)
+    output_img.save(output_path)
+
+
+calibrate.add_command(extract_calibration_image)
 calibrate.add_command(preview)
 
 
