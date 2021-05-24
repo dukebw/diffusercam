9a361dffda4c6047b658447a1c063e80fab9e028
Brendan Duke <brendanw.duke@gmail.com>
2021-05-23 21:27:39 -0400

    Autocorrelation function for PSF

--- src/calibrate.py
+++ src/calibrate.py
@@ -4,17 +4,40 @@ import time
 
 import click
 import numpy as np
+import torch.nn.functional as F
 
 try:
     from picamera import PiCamera
 except ModuleNotFoundError:
     print("No picamera")
 from PIL import Image
+from scipy.signal import fftconvolve
 
 
 @click.group()
 def calibrate():
     pass
+
+
+@click.command()
+@click.option("--psf-path", type=str, required=True)
+def autocorrelate(psf_path):
+    psf_pil = Image.open(psf_path)
+    psf_np = np.array(psf_pil)
+    psf_np = psf_np.sum(axis=2)
+    # background = np.mean(psf_np[5:15, 5:15])
+    # psf_np = psf_np - background
+    print(psf_np.shape)
+    print(f"Pre-normalization: {psf_np.min()}, {psf_np.max()}")
+    psf_np = (psf_np - psf_np.mean()) / psf_np.std()
+    # psf_np = 2.0 * ((psf_np - psf_np.min()) / psf_np.max()) - 1.0
+    print(f"Post-normalization: {psf_np.min()}, {psf_np.max()}")
+    psf_reversed_np = psf_np[::-1, ::-1]
+    psf_autocorr = fftconvolve(psf_np, psf_reversed_np, mode="same")
+    print(f"Autocorrelation {psf_autocorr.min()}, {psf_autocorr.max()}")
+    Image.fromarray(
+        255.0 * ((psf_autocorr - psf_autocorr.min()) / psf_autocorr.max())
+    ).show()
 
 
 @click.command()
@@ -47,6 +70,7 @@ def extract_calibration_image(input_path, output_path):
     output_img.save(output_path)
 
 
+calibrate.add_command(autocorrelate)
 calibrate.add_command(extract_calibration_image)
 calibrate.add_command(preview)
 
